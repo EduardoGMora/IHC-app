3d6406828a956c72a41ff48a0d0b857cd5ed7c84
Emmanuel Martin <manymm00@gmail.com>
2024-11-18 20:47:22 -0600

    Anadi listview.xml y ListViewActivity.java

--- app/src/main/java/com/claseIHC/myapplication2/Splash.java
+++ app/src/main/java/com/claseIHC/myapplication2/Splash.java
@@ -1,13 +1,25 @@
 package com.claseIHC.myapplication2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash); // Conectar el layout XML
+        setContentView(R.layout.splash);
+
+        // Timer para el Splash
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Al terminar el Splash, ir al ListView
+                Intent intent = new Intent(Splash.this, ListViewActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 3000 ms = 3 segundos
     }
 }
